84a93137957c178ab34a505df3fc36885d6e6c34
Mike Gifford <mike.gifford@civicactions.com>
2024-01-08 12:50:03 -0500

    Update run-both-scores.sh

--- sitemap-tools/run-both-scores.sh
+++ sitemap-tools/run-both-scores.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Probably calculate-score.py & find-score.py should be one script. Was just easier for me to build them separately and then mash up the results with bash.
+# Put all these files in the results directory of Purple A11y. Make sure that you can execute ./run-both-scores.sh and that calculate-score.py & find-score.py
+# are in the same directory. 
+# 
+
 # Scan for either todays date or for a match of file name, ie. 20240105
 python find-score.py
 
