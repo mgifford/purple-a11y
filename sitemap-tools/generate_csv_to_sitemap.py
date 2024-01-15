4ff0eb39880741e94aaa7987b6af6cd9b3ceca26
Mike Gifford <mike.gifford@civicactions.com>
2024-01-15 13:06:14 -0500

    Update generate_csv_to_sitemap.py
    Small update

--- sitemap-tools/generate_csv_to_sitemap.py
+++ sitemap-tools/generate_csv_to_sitemap.py
@@ -7,6 +7,9 @@ from xml.etree import ElementTree as ET
 # Define a global count variable to keep track of checked URLs
 url_check_count = 0
 
+# Define a list to store failed URLs
+failed_urls = []
+
 def is_valid_url(url):
     global url_check_count  # Declare the global count variable
 
@@ -21,7 +24,7 @@ def is_valid_url(url):
         return False
 
 def preprocess_url(url):
-    print(f"preprocess_url? {url}")
+    # print(f"preprocess_url? {url}")
     
     # List of URL prefixes to try
     url_prefixes = ["https://www.", "https://", "http://www.", "http://"]
@@ -31,7 +34,7 @@ def preprocess_url(url):
         if is_valid_url(modified_url):
             return modified_url  # Return the modified URL if it's valid
         else:
-            print(f"Failed to load URL with prefix {prefix}: {modified_url}")
+            failed_urls.append(modified_url)  # Add failed URL to the list
     
     return url  # If none of the prefixes worked, return the original URL
 
@@ -40,7 +43,6 @@ def read_csv(csv_file):
     with open(csv_file, 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
         return [row[0].strip() for row in reader]
-
 
 def generate_sitemap(urls, output_file):
     print(f"generate_sitemap? {urls}")
@@ -74,6 +76,12 @@ def main():
 
     generate_sitemap(valid_urls, args.output_file)
     print(f"Sitemap generated with {len(valid_urls)} valid URLs. Saved to {args.output_file}")
+
+    # Print failed URLs
+    if failed_urls:
+        print("\nFailed to load URLs:")
+        for failed_url in failed_urls:
+            print(failed_url)
 
 if __name__ == '__main__':
     main()
