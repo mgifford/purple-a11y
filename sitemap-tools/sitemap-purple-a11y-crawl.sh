f77ed65a4a9c628495fc46f601c1b3ff97af0525
Mike Gifford <mike.gifford@civicactions.com>
2024-01-26 15:01:12 -0500

    Update sitemap-purple-a11y-crawl.sh
    adding option to clean the xml files first

--- sitemap-tools/sitemap-purple-a11y-crawl.sh
+++ sitemap-tools/sitemap-purple-a11y-crawl.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
 
 # Default directory containing .xml files
-sitemap_dir="./sitemaps/"
+sitemap_dir="/Users/mgifford/CA-Sitemap-Scans/sitemap/"
 today_date=$(date +"%d%b%Y") # Adjusted date format
 filter=""
+clean=0  # Flag for cleaning XML files
 
 # Function to display usage
 usage() {
-    echo "Usage: $0 [ -d | -a | -s <string> ]"
+    echo "Usage: $0 [ -d | -a | -s <string> | -c ]"
     echo "  -d: Select files with today's date."
     echo "  -a: Select all files."
     echo "  -s <string>: Select files containing a specific string."
+    echo "  -c: Clean XML files before processing."
     exit 1
 }
 
 # Parse command-line options
-while getopts ":das:" opt; do
+while getopts ":das:c" opt; do
     case ${opt} in
         d ) filter="$today_date";;
         a ) filter="";;
         s ) filter="$OPTARG";;
+        c ) clean=1;;
         \? ) usage;;
     esac
 done
-echo $filter
 
 # Iterate over each .xml file in the directory based on the filter
 for file in "$sitemap_dir"*"$filter"*.xml; do
-    echo $file
     # Check if the file is a regular file
     if [ -f "$file" ]; then
-        # Run the provided command for each file
+        # Run these commands for each file
+        if [ "$clean" -eq 1 ]; then
+            echo "Cleaning $file..."
+            python ../update_sitemap.py -x "$file"
+        fi
+        echo "Processing $file..."
         node cli.js -c 1 -k MikeGifford:mike.gifford@civicactions.com -p 2000 -u "file://$file"
     fi
 done
