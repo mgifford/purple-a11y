468f23cba312cb17b1ba728b83428de7cab461c6
Mike Gifford <mike.gifford@civicactions.com>
2024-01-29 17:31:40 +0100

    Create build-scores.sh

---
+++ sitemap-tools/build-scores.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get today's date in YYYYMMDD format
+DATE_TODAY=$(date +%Y%m%d)
+
+# Run the Python script with the formatted date
+python find-score.py -p $DATE_TODAY -o summary
+
+# Aggregate that into a summary value
+python calculate-score.py -d summary 
+
+# Aggregate the calcualated scores
+python aggregate_scores.py -d summary%     
