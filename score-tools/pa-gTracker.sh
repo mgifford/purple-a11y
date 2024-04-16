f9a4929342a6bad3fd5a0e07f1ccbf510968cfb2
Mike Gifford <mike.gifford@civicactions.com>
2024-04-16 23:02:23 +0200

    Update pa-gTracker.sh
    Adding time feature and also starting with date

--- score-tools/pa-gTracker.sh
+++ score-tools/pa-gTracker.sh
@@ -9,6 +9,9 @@ yamlConfigPath="./pa-gTracker.yml"
 # Correcting the iteration approach
 siteKeys=$(yq e 'keys | .[]' $yamlConfigPath)
 
+# Start the timer
+start=$(date +%s.%N)
+
 for siteKey in $siteKeys; do
     counter=$((counter+1))
     echo "\nProcessing site ($counter): $siteKey\n"
@@ -23,11 +26,28 @@ for siteKey in $siteKeys; do
         max=$(yq e ".$siteKey.[$row].max" $yamlConfigPath)
         sheet_id=$(yq e ".$siteKey.[$row].sheet_id" $yamlConfigPath)
         exclude=$(yq e ".$siteKey.[$row].exclude" $yamlConfigPath)
+        start_date=$(yq e ".$siteKey.[$row].start_date" $yamlConfigPath)
+
 
         # Debugging print
         echo "Run: node --max-old-space-size=6000  $nodeScriptPath --type $type --name $name --url $url --max $max --sheet_id $sheet_id --exclude $exclude"
         
         # Run the Node.js script with these parameters (uncomment when ready) 
-        node --max-old-space-size=6000  "$nodeScriptPath" --type "$type" --name "$name" --url "$url" --max "$max" --sheet_id "$sheet_id"  --exclude "$exclude" 
+        timeout 1h node --max-old-space-size=6000  "$nodeScriptPath" --type "$type" --name "$name" --url "$url" --max "$max" --sheet_id "$sheet_id"  --exclude "$exclude" 
+        
+        # Print the day of the week
+        day_of_week=$(date +%A)
+        echo "Day of the week: $start_date & today is $day_of_week"
     done
+
+    # Calculate the execution time
+    end=$(date +%s.%N)
+    duration=$(echo "$end - $start" | bc)
+    duration_h=$(echo "$duration / 3600" | bc)
+    duration_m=$(echo "($duration % 3600) / 60" | bc)
+    duration_s=$(echo "$duration % 60" | bc)
+    duration_formatted=$(printf "%02d:%02d:%02d" "$duration_h" "$duration_m" "$duration_s")
+
+    # Print the execution time
+    echo "Execution time: $duration_formatted"
 done
