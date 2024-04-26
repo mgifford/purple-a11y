14e2988a836b8427e3d349baecf3f0ae97a16bcb
Mike Gifford <mike.gifford@civicactions.com>
2024-04-26 17:05:34 +0200

    Update pa-gTracker.sh
    Adding date logic.

--- score-tools/pa-gTracker.sh
+++ score-tools/pa-gTracker.sh
@@ -8,11 +8,21 @@ yamlConfigPath="./pa-gTracker.yml"
 yqPath="/opt/homebrew/bin/yq"
 timeoutPath="/opt/homebrew/bin/timeout"
 
-# Correcting the iteration approach
-siteKeys=$($yqPath e 'keys | .[]' $yamlConfigPath)
+
+# Check if the -c flag is provided and overwrite the default path if so
+while getopts "c:" flag; do
+    case $flag in
+        c) yamlConfigPath="$OPTARG";;
+    esac
+done
+shift $((OPTIND-1))
+
 
 # Start the timer
 start=$(date +%s.%N)
+
+# Correcting the iteration approach
+siteKeys=$($yqPath e 'keys | .[]' $yamlConfigPath)
 
 for siteKey in $siteKeys; do
     counter=$((counter+1))
@@ -34,6 +44,18 @@ for siteKey in $siteKeys; do
         start_date=$($yqPath e ".$siteKey.[$row].start_date" $yamlConfigPath)
         strategy=$($yqPath e ".$siteKey.[$row].strategy" $yamlConfigPath)
 
+        # Get today's day of the week
+        today=$(date +%A)
+        time=$(date +%H:%M:%S)
+        # Check if start_date is equal to today's day
+        if [ "$start_date" = "$today" ]; then
+            echo "Continuing as start_date matches today's day. Time: $time"
+
+        else
+            echo "$name - $start_date skipping to next element..."
+            continue
+        fi
+
         if [ $type = "break" ]; then
             echo "Stopping program execution..."
             exit
@@ -45,10 +67,7 @@ for siteKey in $siteKeys; do
         
         # Run the Node.js script with these parameters (uncomment when ready) 
         "$timeoutPath" 1h node --max-old-space-size=6000  "$nodeScriptPath" --type "$type" --name "$name" --url "$url" --max "$max" --sheet_id "$sheet_id"  --exclude "$exclude" --strategy "$strategy"
-        
-        # Print the day of the week
-        day_of_week=$(date +%A)
-        echo "Day of the week: $start_date & today is $day_of_week"
+
     done
 
     # Capture the end time for each site
@@ -56,16 +75,10 @@ for siteKey in $siteKeys; do
 
     # Calculate the execution time for each site
     duration=$(echo "$end - $start" | bc)
-
-    # Convert to seconds (integer), ignoring sub-second precision for simplicity
     duration_s=${duration%.*}
-    
-    # Calculate hours, minutes, and seconds
     let duration_h=duration_s/3600
     let duration_m=(duration_s%3600)/60
     let duration_s=(duration_s%60)
-
-    # Format and print the execution time
     duration_formatted=$(printf "%02dh:%02dm:%02ds" $duration_h $duration_m $duration_s)
     echo "Execution time: $duration_formatted"
 done
